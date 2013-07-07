6a033cf828117588a38cd3d0553fe55d26673113
kaishengyao <ksyao@hotmail.com>
2013-07-07 12:54:57 -0700

    corrected grader.js

--- grader.js
+++ grader.js
@@ -1,33 +1,19 @@
 #!/usr/bin/env node
+
 /*
-Automatically grade files for the presence of specified HTML tags/attributes.
-Uses commander.js and cheerio. Teaches command line application development
-and basic DOM parsing.
-
-References:
-
- + cheerio
-   - https://github.com/MatthewMueller/cheerio
-   - http://encosia.com/cheerio-faster-windows-friendly-alternative-jsdom/
-   - http://maxogden.com/scraping-with-node.html
-
- + commander.js
-   - https://github.com/visionmedia/commander.js
-   - http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy
-
- + JSON
-   - http://en.wikipedia.org/wiki/JSON
-   - https://developer.mozilla.org/en-US/docs/JSON
-   - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
-
- To be modified
+--checks checks.json --url http://warm-refuge-7653.herokuapp.com
 */
 
-var fs = require('fs');
 var program = require('commander');
+var fs = require('fs');
 var cheerio = require('cheerio');
+
+var sys = require('util');
+var rest = require('restler');
+
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var HTMLCOPY_DEFAULT = "./index.html.cp";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -36,6 +22,12 @@ var assertFileExists = function(infile) {
         process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
     }
     return instr;
+};
+
+var clone = function(fn) {
+    // Workaround for commander.js issue.
+    // http://stackoverflow.com/a/6772648
+    return fn.bind({});
 };
 
 var cheerioHtmlFile = function(htmlfile) {
@@ -57,20 +49,37 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
-var clone = function(fn) {
-    // Workaround for commander.js issue.
-    // http://stackoverflow.com/a/6772648
-    return fn.bind({});
-};
-
 if(require.main == module) {
+    var csvfile = HTMLCOPY_DEFAULT;
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url_file>', 'Path to url')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+
+    var checkJson;
+    if (program.file) {
+	csvfile = program.file;
+//	console.log(' program file = %s ', csvfile);
+    }
+    if (program.url){
+//	console.log(' url = %s ', program.url);
+	
+	rest.get(program.url).on('complete', function(result) {
+	    var csvfile = HTMLCOPY_DEFAULT;
+	    if (result instanceof Error) {
+		sys.puts('Error: ' + result.message);
+		this.retry(5000); // try again after 5 sec
+	    } else {
+		fs.writeFileSync(csvfile, result);
+	    }
+	});
+    }
+    checkJson = checkHtmlFile(csvfile, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
+
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
+
