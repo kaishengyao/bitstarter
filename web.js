4fdf7d5a33dbeb53e438950ae9a8a95f86cb2580
kaishengyao <ksyao@hotmail.com>
2013-07-06 21:52:42 -0700

    modified web.js

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var data = fs.readFileSync("index.html")
+
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+//  response.send('Hello World!');
+  response.send(data.toString());
 });
 
 var port = process.env.PORT || 5000;
